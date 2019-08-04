15e284a14d42cb3d4b966de29b2c769e395442e1
Rupade <arupade@internal.tavisca.com>
2019-08-04 15:37:37 +0530

    Initial commit

---
+++ MyWebServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.IO;
+
+namespace MyWebServer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+    }
+}
---
+++ MyWebServer/WebServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Text;
+using System.IO;
+
+namespace MyWebServer
+{
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public WebServer( Func<HttpListenerRequest, string> method,params string[] prefixes)
+        {
+
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            if (method == null)
+                throw new ArgumentException("method");
+
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                           
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { }
+                            finally
+                            {
+                               
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { }
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+}
